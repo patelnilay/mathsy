4dbba31fed23dfc00a2fb95a865470a370b0e6b0
Nilay Patel <nilay.patel@ada.ac.uk>
2019-05-19 09:36:13 +0100

    added features like the buttons and new pages

--- lib/main.dart
+++ lib/main.dart
@@ -1,113 +1,328 @@
 import 'package:flutter/material.dart';
-import 'package:english_words/english_words.dart';
+import 'package:flutter/services.dart';
 
-void main() => runApp(new MyApp());
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return new MaterialApp(
-      title: 'Startup Name Generator',
-      theme: new ThemeData(
-        primaryColor: Colors.blue,
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitUp,
+    ]);
+    return MaterialApp(
+      title: 'Mathsy',
+      theme: ThemeData(
+        primaryColor: Colors.amber,
+        accentColor: Colors.teal[600],
+        fontFamily: 'Raleway',
       ),
-      home: new RandomWords(),
+      home: StartPage(),
     );
   }
 }
 
-class RandomWords extends StatefulWidget {
-  @override
-  RandomWordsState createState() => new RandomWordsState();
-}
-
-class RandomWordsState extends State<RandomWords> {
-  final List<WordPair> _suggestions = <WordPair>[];
-  final Set<WordPair> _saved = new Set<WordPair>();
-  final TextStyle _biggerFont = const TextStyle(fontSize: 18.0);
-
-  void _pushSaved() {
-    Navigator.of(context).push(
-      new MaterialPageRoute<void>(
-        builder: (BuildContext context) {
-          final Iterable<ListTile> tiles = _saved.map(
-                (WordPair pair) {
-              return new ListTile(
-                title: new Text(
-                  pair.asPascalCase,
-                  style: _biggerFont,
-                ),
-              );
-            },
-          );
-          final List<Widget> divided = ListTile.divideTiles(
-            context: context,
-            tiles: tiles,
-          ).toList();
-
-          return new Scaffold(
-            appBar: new AppBar(
-              title: const Text('Saved Suggestions'),
-            ),
-            body: new ListView(children: divided),
-          );
-        },
-      ),
-    );
-  }
-
+class StartPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return new Scaffold(
-      appBar: new AppBar(
-        title: const Text('Startup Name Generator'),
-        actions: <Widget>[
-          new IconButton(icon: const Icon(Icons.list), onPressed: _pushSaved),
-        ],
+    return Scaffold(
+      body: Center(
+        child: Column(
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.only(top: 50),
+              child: Text(
+                "Mathsy",
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 50,
+                  fontFamily: 'Raleway',
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 50),
+              child: Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(30.0),
+                  gradient: LinearGradient(
+                    begin: Alignment.topLeft,
+                    end: Alignment.bottomRight,
+                    colors: <Color>[
+                      Color(0xFFFF8F00),
+                      Color(0xFFFFAF00),
+                      Color(0xFFFFC107),
+                    ],
+                  ),
+                ),
+                child: FlatButton(
+                  padding: const EdgeInsets.only(
+                      top: 20.0, bottom: 20.0, left: 80.0, right: 80.0),
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => DifficultyPage()),
+                    );
+                  },
+                  child: Text(
+                    "START!",
+                    style: TextStyle(fontFamily: 'Raleway'),
+                  ),
+                  textColor: Colors.white,
+                  shape: new RoundedRectangleBorder(
+                      borderRadius: new BorderRadius.circular(30.0)),
+                ),
+              ),
+            ),
+            Expanded(
+              child: Image(
+                  alignment: Alignment.bottomCenter,
+                  image: AssetImage('graphics/start_page.png')),
+            ),
+          ],
+        ),
       ),
-      body: _buildSuggestions(),
     );
   }
+}
 
-  Widget _buildSuggestions() {
-    return new ListView.builder(
-        padding: const EdgeInsets.all(16.0),
-        itemBuilder: (BuildContext _context, int i) {
-          if (i.isOdd) {
-            return const Divider();
-          }
-          final int index = i ~/ 2;
-          if (index >= _suggestions.length) {
-            _suggestions.addAll(generateWordPairs().take(10));
-          }
-          return _buildRow(_suggestions[index]);
-        });
+class DifficultyPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Center(
+      child: Column(
+        children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.only(top: 50),
+            child: Text(
+              "Mathsy",
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 50,
+                fontFamily: 'Raleway',
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(top: 50),
+            child: Container(
+              width: 220.0,
+              height: 72.0,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(30.0),
+                gradient: LinearGradient(
+                  begin: Alignment.topLeft,
+                  end: Alignment.bottomRight,
+                  colors: <Color>[
+                    Color(0xFFFF8F00),
+                    Color(0xFFFFAF00),
+                    Color(0xFFFFC107),
+                  ],
+                ),
+              ),
+              child: FlatButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => QuestionsPage()),
+                  );
+                },
+                child: Text(
+                  "EASY",
+                  style: TextStyle(
+                    fontFamily: 'Raleway',
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                textColor: Colors.white,
+                shape: new RoundedRectangleBorder(
+                    borderRadius: new BorderRadius.circular(30.0)),
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(top: 30),
+            child: Container(
+              width: 220.0,
+              height: 72.0,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(30.0),
+                gradient: LinearGradient(
+                  begin: Alignment.topLeft,
+                  end: Alignment.bottomRight,
+                  colors: <Color>[
+                    Color(0xFFFF8F00),
+                    Color(0xFFFFAF00),
+                    Color(0xFFFFC107),
+                  ],
+                ),
+              ),
+              child: FlatButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => QuestionsPage()),
+                  );
+                },
+                child: Text(
+                  "MEDIUM",
+                  style: TextStyle(
+                    fontFamily: 'Raleway',
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                textColor: Colors.white,
+                shape: new RoundedRectangleBorder(
+                    borderRadius: new BorderRadius.circular(30.0)),
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(top: 30),
+            child: Container(
+              width: 220.0,
+              height: 72.0,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(30.0),
+                gradient: LinearGradient(
+                  begin: Alignment.topLeft,
+                  end: Alignment.bottomRight,
+                  colors: <Color>[
+                    Color(0xFFFF8F00),
+                    Color(0xFFFFAF00),
+                    Color(0xFFFFC107),
+                  ],
+                ),
+              ),
+              child: FlatButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => QuestionsPage()),
+                  );
+                },
+                child: Text(
+                  "HARD",
+                  style: TextStyle(
+                    fontFamily: 'Raleway',
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                textColor: Colors.white,
+                shape: new RoundedRectangleBorder(
+                    borderRadius: new BorderRadius.circular(30.0)),
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(top: 30),
+            child: Container(
+              width: 220.0,
+              height: 72.0,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(30.0),
+                gradient: LinearGradient(
+                  begin: Alignment.topLeft,
+                  end: Alignment.bottomRight,
+                  colors: <Color>[
+                    Color(0xFFC62828),
+                    Color(0xFFD32F2F),
+                    Color(0xFFE53935),
+                  ],
+                ),
+              ),
+              child: FlatButton(
+                onPressed: () {
+                  Navigator.pop(
+                    context,
+                    MaterialPageRoute(builder: (context) => StartPage()),
+                  ); // Navigate back to first route when tapped.
+                },
+                child: Text(
+                  'BACK',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontFamily: 'Raleway',
+                  ),
+                ),
+                textColor: Colors.white,
+                shape: new RoundedRectangleBorder(
+                    borderRadius: new BorderRadius.circular(30.0)),
+              ),
+            ),
+          ),
+        ],
+      ),
+    ));
   }
+}
 
-  Widget _buildRow(WordPair pair) {
-    final bool alreadySaved = _saved.contains(pair);
-    return new ListTile(
-      leading: CircleAvatar(
-        child: Text(pair.toString().substring(0, 1).toUpperCase()),
-        foregroundColor: Colors.white,
-        backgroundColor: Colors.blue,
-      ),
-      trailing: new Icon(
-        alreadySaved ? Icons.favorite : Icons.favorite_border,
-        color: alreadySaved ? Colors.red : null,
-      ),
-      onTap: () {
-        setState(() {
-          if (alreadySaved) {
-            _saved.remove(pair);
-          } else {
-            _saved.add(pair);
-          }
-        });
-      },
-      title: new Text(
-        pair.asCamelCase,
-        style: _biggerFont,
+class QuestionsPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          children: <Widget>[
+            Padding(
+              padding: EdgeInsets.only(top: 40.0),
+              child: Text(
+                "N+M",
+                style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontFamily: 'Raleway',
+                    fontSize: 50),
+              ),
+            ),
+            Container(
+              width: 200,
+              padding: EdgeInsets.only(top: 30),
+              child: TextField(
+                    decoration: InputDecoration(
+                        hintText: 'Please enter a search term'
+                    ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 30),
+              child: Container(
+                width: 200.0,
+                height: 52.0,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(30.0),
+                  gradient: LinearGradient(
+                    begin: Alignment.topLeft,
+                    end: Alignment.bottomRight,
+                    colors: <Color>[
+                      Color(0xFFFF8F00),
+                      Color(0xFFFFAF00),
+                      Color(0xFFFFC107),
+                    ],
+                  ),
+                ),
+                child: FlatButton(
+                  onPressed: () {
+                    Navigator.pop(
+                      context,
+                      MaterialPageRoute(builder: (context) => DifficultyPage()),
+                    ); // Navigate back to first route when tapped.
+                  },
+                  child: Text(
+                    'BACK',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontFamily: 'Raleway',
+                    ),
+                  ),
+                  textColor: Colors.white,
+                  shape: new RoundedRectangleBorder(
+                      borderRadius: new BorderRadius.circular(30.0)),
+                ),
+              ),
+            ),
+
+          ],
+        ),
       ),
     );
   }
