2c25b2b0033cd35336be947eab6b2b087f7d3a1b
Nilay Patel <nilay.patel@ada.ac.uk>
2019-05-21 20:13:04 +0100

    added new fonts and also made the question page a stateless widget to allow for new questions to be generated!

--- lib/questions_page.dart
+++ lib/questions_page.dart
@@ -1,12 +1,109 @@
+//import 'package:flutter/material.dart';
+//import 'package:mathsy/question_generator.dart';
+//
+//class QuestionWidget extends StatefulWidget {
+//  @override
+//  _QuestionWidgetState createState() => _QuestionWidgetState();
+//}
+//
+//class _QuestionWidgetState extends State<QuestionWidget> {
+//  Question question;
+//
+//  @override
+//  void initState() {
+//    question = QuestionsGenerator.generateQuestion();
+//    super.initState();
+//  }
+//
+//  @override
+//  Widget build(BuildContext context) {
+//    return Padding(
+//      padding: EdgeInsets.only(top: 40.0),
+//      child: Text(
+//        "${question.phrase}",
+//        style: TextStyle(
+//            fontWeight: FontWeight.bold,
+//            fontFamily: 'Raleway',
+//            fontSize: 50),
+//      ),
+//    );
+//  }
+//}
+//
+//class QuestionsPage extends StatefulWidget {
+//  @override
+//  _QuestionsPageState createState() => _QuestionsPageState();
+//}
+//
+//class _QuestionsPageState extends State<QuestionsPage> {
+//  @override
+//  Widget build(BuildContext context) {
+//    return Scaffold(
+//      body: Center(
+//        child: Column(
+//          children: <Widget>[
+//            QuestionWidget(),
+//            Container(
+//              width: 200,
+//              padding: EdgeInsets.only(top: 30),
+//              child: TextField(
+//                decoration: InputDecoration(
+//                    hintText: 'Answer Here'
+//                ),
+//                keyboardType: TextInputType.number,
+//                style: TextStyle(fontSize: 30),
+//              ),
+//            ),
+//            Padding(
+//              padding: const EdgeInsets.only(top: 30),
+//              child: Container(
+//                width: 200.0,
+//                height: 52.0,
+//                decoration: BoxDecoration(
+//                  borderRadius: BorderRadius.circular(30.0),
+//                  gradient: LinearGradient(
+//                    begin: Alignment.topLeft,
+//                    end: Alignment.bottomRight,
+//                    colors: <Color>[
+//                      Color(0xFFFF8F00),
+//                      Color(0xFFFFAF00),
+//                      Color(0xFFFFC107),
+//                    ],
+//                  ),
+//                ),
+//                child: FlatButton(
+//                  onPressed: () {
+//                    print(context.ancestorStateOfType(TypeMatcher<QuestionsPage>()));
+//                  },
+//                  child: Text(
+//                    'SUBMIT',
+//                    style: TextStyle(
+//                      fontWeight: FontWeight.bold,
+//                      fontFamily: 'Raleway',
+//                    ),
+//                  ),
+//                  textColor: Colors.white,
+//                  shape: new RoundedRectangleBorder(
+//                      borderRadius: new BorderRadius.circular(30.0)),
+//                ),
+//              ),
+//            ),
+//
+//          ],
+//        ),
+//      ),
+//    );
+//  }
+//}
 import 'package:flutter/material.dart';
 import 'package:mathsy/question_generator.dart';
 
-class QuestionWidget extends StatefulWidget {
+class QuestionsPage extends StatefulWidget {
   @override
-  _QuestionWidgetState createState() => _QuestionWidgetState();
+  _QuestionsPageState createState() => _QuestionsPageState();
 }
 
-class _QuestionWidgetState extends State<QuestionWidget> {
+class _QuestionsPageState extends State<QuestionsPage> {
   Question question;
 
   @override
@@ -15,47 +112,35 @@ class _QuestionWidgetState extends State<QuestionWidget> {
     super.initState();
   }
 
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: EdgeInsets.only(top: 40.0),
-      child: Text(
-        "${question.phrase}",
-        style: TextStyle(
-            fontWeight: FontWeight.bold,
-            fontFamily: 'Raleway',
-            fontSize: 50),
-      ),
-    );
-  }
-}
-
-class QuestionsPage extends StatefulWidget {
-  @override
-  _QuestionsPageState createState() => _QuestionsPageState();
-}
-
-class _QuestionsPageState extends State<QuestionsPage> {
-  @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
         child: Column(
           children: <Widget>[
-            QuestionWidget(),
-            Container(
-              width: 200,
-              padding: EdgeInsets.only(top: 30),
-              child: TextField(
-                decoration: InputDecoration(
-                    hintText: 'Answer Here'
+            Padding(
+              padding: const EdgeInsets.only(top: 60),
+              child: Text(
+                "${question.phrase}",
+                style: TextStyle(
+//                  fontWeight: FontWeight.bold,
+                  fontSize: 90,
+                  fontFamily: 'Lobster-Two',
                 ),
+                textAlign: TextAlign.center,
+              ),
+            ),
+            Container(
+              width: 220,
+              padding: EdgeInsets.only(top: 60),
+              child: TextField(
+                decoration: InputDecoration(hintText: 'Answer Here'),
                 keyboardType: TextInputType.number,
                 style: TextStyle(fontSize: 30),
+                textAlign: TextAlign.center,
               ),
             ),
             Padding(
-              padding: const EdgeInsets.only(top: 30),
+              padding: const EdgeInsets.only(top: 60),
               child: Container(
                 width: 200.0,
                 height: 52.0,
@@ -73,7 +158,9 @@ class _QuestionsPageState extends State<QuestionsPage> {
                 ),
                 child: FlatButton(
                   onPressed: () {
-                    print(context.ancestorStateOfType(TypeMatcher<QuestionsPage>()));
+                    setState(() {
+                      question = QuestionsGenerator.generateQuestion();
+                    });
                   },
                   child: Text(
                     'SUBMIT',
@@ -88,7 +175,6 @@ class _QuestionsPageState extends State<QuestionsPage> {
                 ),
               ),
             ),
-
           ],
         ),
       ),
